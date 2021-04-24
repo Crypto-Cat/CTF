db30554dea4d3cd08cf1a25fba89c29ea4ab2475
Crypto-Cat <jburgess03@qub.ac.uk>
2021-04-24 16:03:31 +0100

    Added short pwntools script to perform basic XOR

---
+++ HTB/cyber_apocalypse_21/phasestream/phasestream1.py
@@ -0,0 +1,7 @@
+from pwn import *
+
+# phasestream1
+ciphertext = unhex("2e313f2702184c5a0b1e321205550e03261b094d5c171f56011904")
+key = xor(ciphertext[0:5], "CHTB{")
+plaintext = xor(ciphertext, key)
+success('Phasestream1 Decrypted: %s', plaintext)
